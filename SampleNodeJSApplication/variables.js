cebb408fd0ac0e70885114079dc77b40956c894d
athirarajan20 <124857931+athirarajan20@users.noreply.github.com>
2023-02-09 10:11:15 +0530

    Add files via upload

---
+++ SampleNodeJSApplication/variables.js
@@ -0,0 +1,24 @@
+
+console.log("HELLO WORLD")
+var id = 100
+
+console.log("id = " ,id)
+let a=5
+let b=3
+console.log("sum of a,b is:" ,a+b)
+
+let name1="Athira"
+console.log(name1);
+
+let bool_val=true
+console.log(bool_val)
+
+let array_val=val=[1,2,3]
+array_val="changed value" //no restriction in data type change and values
+console.log(array_val)
+
+let obj={name:"Athira",value:"MCA"}
+console.log(obj)
+
+
+ 
