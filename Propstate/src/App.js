7f795ab6cdce89d28b538b6ba68bcc3f83616443
athirarajan20 <124857931+athirarajan20@users.noreply.github.com>
2023-03-06 11:20:20 +0530

    Add files via upload

---
+++ Propstate/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/class-comp/Welcome';
+import ParentComponent from './components/function-comp/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
